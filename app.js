9efc44bc85848d9ad8bc89378edad6e57765d788
subhranil2002 <137594629+subhranil2002@users.noreply.github.com>
2024-05-16 01:21:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('dashboard.html');
+
+var SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+var port = new SerialPort('/dev/cu.usbmodem11201', {
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+port.pipe(parser);
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end(index);
+});
+
+var io = require('socket.io')(server); // Corrected line to create socket.io server
+
+io.on('connection', function(socket) {
+    console.log('Node is listening to port');
+});
+
+parser.on('data', function(data) {
+    console.log('Received data from port: ' + data);
+    io.emit('data', data);
+});
+
+server.listen(3001, function() {
+    console.log('Server is running on port 3000');
+});
